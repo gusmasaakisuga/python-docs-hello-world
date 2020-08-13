8deb598a0b552316cbb5f0d62ce600945f06f117
Gus Masaaki Suga <masaaki.suga@gmail.com>
2020-08-13 16:40:35 +0900

    added more words

--- application.py
+++ application.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     print('hello method invoked')
-    return "Hello Azure! from Python"
+    return "Hello Azure! from Python.  Let's see what you can do."
